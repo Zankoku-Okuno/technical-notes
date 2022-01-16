76010d9d272cb79c35c07f82a1ab68764414f6d0
Okuno Zankoku <zankoku.okuno@gmail.com>
2022-01-15 23:00:44 -0500

    build script and first entry as test case

---
+++ build.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import sys
+
+from collections import defaultdict as ddict
+from glob import glob
+from os import path
+
+# TODO move this into a separately-loaded file
+tagwordToTitle = {
+  "hott": "HoTT",
+  "todo": "TODO",
+}
+
+byTag = ddict(lambda: []) # {tag: [(title, filename), …], …}
+untagged = [] # [(title, fname), …]
+tagCount = ddict(lambda: 0) # {tag: <int>, … }
+
+def main():
+  tagRegex = r'[a-z-]+'
+  tagMatch = r'tag:\s+('+tagRegex+r')\s*'
+  tagsMatch = r'tag:\s+('+tagRegex+r'(?:\s*,\s+'+tagRegex+r')*)(?:\s*,)?\s*'
+
+  os.chdir(path.realpath(path.dirname(__file__)))
+  for fname in glob("./notes/*.md"):
+    title, tags = processNote(fname)
+    if tags:
+      for tag in tags:
+        byTag[tag].append((title, fname))
+        tagCount[tag] += 1
+    else:
+      print(f"[WARN] no tags for {fname}", file=sys.stderr)
+      untagged.append((title, fname))
+
+  with open("index.md", 'wt') as fp:
+    print("# Technical Notes Index", file=fp)
+    print("", file=fp)
+    generateTagIndex(fp)
+  with open("tags.txt", 'wt') as fp:
+    counts = sorted(
+        ((tag, len(byTag[tag])) for tag in byTag.keys())
+      , key=lambda x: (-x[1], x[0]) # ordered by descending frequency, then ascending name
+      )
+    for tag, count in counts:
+      print(f"{tag} {count}", file=fp)
+
+def generateTagIndex(fp):
+  out = lambda msg: print(msg, file=fp)
+  out("## Notes by Tag")
+  out("")
+  for tag in sorted(byTag.keys()):
+    humanTag = " ".join(toTagword(tag) for tag in tag.split("-"))
+    out(f"### {humanTag}")
+    out("")
+    for title, fname in byTag[tag]:
+      out(f"  * [{title}]({fname})")
+    out("")
+  if untagged:
+    out("### WARNING: Untagged Notes")
+    out("")
+    for title, fname in untagged:
+      out(f"  * [{title}]({fname})")
+    out("")
+  out("")
+
+def toTagword(tagpart):
+  return tagwordToTitle.get(tagpart, tagpart.title())
+
+
+def processNote(fname):
+  (title, _) = path.splitext(fname)
+  tags = set()
+  with open(fname, 'rt') as fp:
+    state = 'titleSearch'
+    for line in fp.readlines():
+      if state == 'titleSearch':
+        if re.match(r'^#\s*\S+', line):
+          title = line[1:].strip()
+          state = 'tagSearch'
+          continue
+        # skip blank lines
+        elif not line.strip():
+          continue
+        # no title in file
+        else:
+          state = 'tagSearch'
+      # I'm using an `if` here b/c then `titleSearch` can fall-through when it transitions to `tagSearch`
+      if state == 'titleSearch':
+        pass # already processed
+      elif state == 'tagSearch':
+        # skip blank lines
+        if not line.strip():
+          continue
+        tagRegex = r'[a-z]+(?:-[a-z]+|\d+)*'
+        tagMatch = re.match(r'tag:\s+('+tagRegex+r')\s*', line)
+        tagsMatch = re.match(r'tags:\s+('+tagRegex+r'(?:\s*,\s+'+tagRegex+r')*)(?:\s*,\s*)?\s*', line)
+        # extract tag and tags lines
+        if tagMatch:
+          tags.add(tagMatch.group(1))
+        elif tagsMatch:
+          tags |= set(tag.strip() for tag in tagsMatch.group(1).split(","))
+        else:
+          break
+      else:
+        raise Exception(f"internal error: unknown state {repr(state)}")
+    return title, tags
+
+if __name__ == "__main__":
+  main()
---
+++ index.md
@@ -0,0 +1,26 @@
+# Technical Notes Index
+
+## Notes by Tag
+
+### HoTT
+
+  * [Design Goals for a HoTT-based Language](./notes/2020-01-15-01-hottlang.md)
+
+### Lang Design
+
+  * [Design Goals for a HoTT-based Language](./notes/2020-01-15-01-hottlang.md)
+  * [Language Development Plans](./notes/2022-01-16-00-plans.md)
+
+### Parsers
+
+  * [Language Development Plans](./notes/2022-01-16-00-plans.md)
+
+### Plan
+
+  * [Language Development Plans](./notes/2022-01-16-00-plans.md)
+
+### TODO
+
+  * [Design Goals for a HoTT-based Language](./notes/2020-01-15-01-hottlang.md)
+
+
